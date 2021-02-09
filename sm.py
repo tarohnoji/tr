0227029c500c956f0979bd173b76c3ecae2f07b3
tarohnoji <tailangyede9@gmail.com>
2021-02-10 05:15:11 +0900

    modified code

--- sm.py
+++ sm.py
@@ -4,4 +4,10 @@ for i in range(20):
     if i % 2 == 0:
         print(i)
     else:
-        print('0')
+        print('0'i)
+
+my_list = [1, 2, 3, 4, 5]
+
+for num in my_list:
+    print(num*num)
+
